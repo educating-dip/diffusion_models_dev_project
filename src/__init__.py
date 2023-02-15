8b31d60e40fde585a8991c51e5beb689e8d5f5c6
adenker <adenker@uni-bremen.de>
2023-02-15 11:25:11 +0100

    import openai model

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 from .dataset import EllipseDatasetFromDival
 from .utils import marginal_prob_std, diffusion_coeff, loss_fn, ExponentialMovingAverage
-from .third_party_models import ScoreNet
+from .third_party_models import ScoreNet, UNetModel
 from .samplers import pc_sampler
