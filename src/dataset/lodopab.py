e0f33e2eed0a6248cff8bbc541e0a5d3c1e8c6de
adenker <adenker@uni-bremen.de>
2023-06-14 09:03:03 +0200

    added support to original LoDoPabCT dataset

--- src/dataset/lodopab.py
+++ src/dataset/lodopab.py
@@ -10,33 +10,76 @@ class LoDoPabDatasetFromDival():
     def __init__(self, 
         impl: str = 'astra_cuda',
         im_size: int = 362,
-        use_transform: bool = True
         ) -> None:
 
         self.impl = impl
         dataset = get_standard_dataset('lodopab', impl=self.impl)
         self.ray_trafo = dataset.ray_trafo
         self.im_size = im_size
-        self.use_transform = use_transform
+        self.use_transform = self.im_size != 362
         def transform(sample: Tensor) -> Tensor:
             x = sample[1]
-            x = interpolate(
-                    x.unsqueeze(0), 
-                    size=(self.im_size, self.im_size), 
-                    mode='bilinear'
-                ).squeeze()
+            if self.use_transform:
+                x = interpolate(
+                        x.unsqueeze(0), 
+                        size=(self.im_size, self.im_size), 
+                        mode='bilinear'
+                    ).squeeze().unsqueeze(0) 
 
-            return x.unsqueeze(0) 
+            return x
 
         self.lodopab_train = dataset.create_torch_dataset(part='train',
             reshape=((1,) + dataset.space[0].shape,
-            (1,) + dataset.space[1].shape), transform=transform if self.use_transform else None)
+            (1,) + dataset.space[1].shape), transform=transform)
         self.lodopab_val = dataset.create_torch_dataset(part='validation',
             reshape=((1,) + dataset.space[0].shape,
-            (1,) + dataset.space[1].shape), transform=transform if self.use_transform else None)
+            (1,) + dataset.space[1].shape), transform=transform)
         self.lodopab_test = dataset.create_torch_dataset(part='test',
             reshape=((1,) + dataset.space[0].shape,
-            (1,) + dataset.space[1].shape), transform=transform if self.use_transform else None)
+            (1,) + dataset.space[1].shape), transform=transform)
+
+    def get_trainloader(self,
+            batch_size: int,
+            num_data_loader_workers: int = 0
+            ) -> DataLoader:
+        return DataLoader(self.lodopab_train, 
+            batch_size=batch_size, num_workers=num_data_loader_workers, pin_memory=True, shuffle=True)
+
+    def get_valloader(self,
+            batch_size: int, 
+            num_data_loader_workers: int = 0
+            ) -> DataLoader:
+        return DataLoader(self.lodopab_val, 
+            batch_size=batch_size, num_workers=num_data_loader_workers, pin_memory=True, shuffle=False)
+
+    def get_testloader(self,
+            batch_size: int, 
+            num_data_loader_workers: int = 0
+            ) -> DataLoader:
+        return DataLoader(self.lodopab_test, 
+            batch_size=batch_size, num_workers=num_data_loader_workers, pin_memory=True, shuffle=False)
+
+
+class LoDoPabChallenge():
+    def __init__(self, 
+        impl: str = 'astra_cuda',
+        im_size: int = 362,
+        ) -> None:
+
+        self.impl = impl
+        dataset = get_standard_dataset('lodopab', impl=self.impl)
+        self.ray_trafo = dataset.ray_trafo
+        self.im_size = im_size
+        
+        self.lodopab_train = dataset.create_torch_dataset(part='train',
+            reshape=((1,) + dataset.space[0].shape,
+            (1,) + dataset.space[1].shape))
+        self.lodopab_val = dataset.create_torch_dataset(part='validation',
+            reshape=((1,) + dataset.space[0].shape,
+            (1,) + dataset.space[1].shape))
+        self.lodopab_test = dataset.create_torch_dataset(part='test',
+            reshape=((1,) + dataset.space[0].shape,
+            (1,) + dataset.space[1].shape))
 
     def get_trainloader(self,
             batch_size: int,
