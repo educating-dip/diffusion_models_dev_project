educating-dip/diffusion_models_dev_project

commit fff798e820edd73273c8ba8bc36b68a75ab3873d

only divide by marginal std in unet for score matching

src/utils/exp_utils.py
@@ -25,7 +25,7 @@ def get_standard_score(config, sde, use_ema, load_model=True):
             out_channels=config.model.out_channels,
             num_res_blocks=config.model.num_res_blocks,
             attention_resolutions=config.model.attention_resolutions,
-            marginal_prob_std=sde.marginal_prob_std,
+            marginal_prob_std=sde.marginal_prob_std if sde in [_SCORE_PRED_CLASSES] else None,
             channel_mult=config.model.channel_mult,
             conv_resample=config.model.conv_resample,
             dims=config.model.dims,

src/third_party_models/openai_unet/unet.py
@@ -594,6 +594,8 @@ class OpenAiUNetModel(nn.Module):
             h = module(h, emb)
         h = h.type(x.dtype)
         h = self.out(h)
-        h = h / self.marginal_prob_std(timesteps)[:, None, None, None]
+
+        if not self.marginal_prob_std == None:
+            h = h / self.marginal_prob_std(timesteps)[:, None, None, None]
 
         return h

commit 00ed3efaee6ccb87242f668045ecb258ceeb3288

fix to bug introduced in commit fff798e820edd73273c8ba8bc36b68a75ab3873d

src/samplers/adaptation.py
@@ -40,9 +40,8 @@ def _score_model_adpt(
             if "bias" in name and not "emb_layers" in name:
                 param.requires_grad = True
         inject_trainable_lora_extended(score, **adpt_kwargs)
-
     elif impl == 'dif-fit':
-        pass
+        raise NotImplementedError
     else: 
         raise NotImplementedError
 

src/utils/exp_utils.py
@@ -16,7 +16,6 @@ from ..samplers import (BaseSampler, Euler_Maruyama_sde_predictor, Langevin_sde_
     chain_simple_init, decomposed_diffusion_sampling_sde_predictor, adapted_ddim_sde_predictor, tv_loss, _adapt, _score_model_adpt)
 
 def get_standard_score(config, sde, use_ema, load_model=True):
-
     if config.model.model_name.lower() == 'OpenAiUNetModel'.lower():
         score = OpenAiUNetModel(
             image_size=config.data.im_size,
@@ -25,7 +24,7 @@ def get_standard_score(config, sde, use_ema, load_model=True):
             out_channels=config.model.out_channels,
             num_res_blocks=config.model.num_res_blocks,
             attention_resolutions=config.model.attention_resolutions,
-            marginal_prob_std=sde.marginal_prob_std if sde in [_SCORE_PRED_CLASSES] else None,
+            marginal_prob_std=sde.marginal_prob_std if any([isinstance(sde, classname) for classname in _SCORE_PRED_CLASSES]) else None,
             channel_mult=config.model.channel_mult,
             conv_resample=config.model.conv_resample,
             dims=config.model.dims,