6a2885db338fcca12510c99f58b19594490f7258
adenker <adenker@uni-bremen.de>
2023-06-14 09:03:33 +0200

    added original LoDoPAb forward operator

--- src/physics/__init__.py
+++ src/physics/__init__.py
@@ -1,4 +1,4 @@
-from .trafo import SimpleTrafo
+from .trafo import SimpleTrafo, LoDoPabTrafo
 from .simulation import simulate, SimulatedDataset
 from .walnut_2d_ray_trafo import get_walnut_2d_ray_trafo
 from .base_ray_trafo import BaseRayTrafo
